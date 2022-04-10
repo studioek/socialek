325eda48df30075bc9de300f0b889199234a1fd9
emrekayik <emrekayik1905mail.com>
2022-04-10 13:33:17 +0300

    10.04

---
+++ src/App.svelte
@@ -0,0 +1,67 @@
+<script>
+	import { Router, Route, Link } from "svelte-navigator";
+  import Login from "./route/Login/Login.svelte";
+  import PrivateRoute from "./route/Private/PrivateRoute.svelte";
+  import { user } from "./stores";
+
+  function handleLogout() {
+    $user = null;
+  }
+</script>
+
+<main>
+	<Router>
+		<header>
+		  <h1>History</h1>
+	  
+		  <nav>
+			<Link to="/">Home</Link>
+			<Link to="about">About</Link>
+			<Link to="profile">Profile</Link>
+		  </nav>
+		</header>
+	  
+		<main>
+		  <Route path="login">
+			<Login />
+		  </Route>
+	  
+		  <Route path="/">
+			<h3>Home</h3>
+			<p>Home sweet home...</p>
+		  </Route>
+	  
+		  <Route path="about">
+			<h3>About</h3>
+			<p>That's what it's all about!</p>
+		  </Route>
+	  
+		  <PrivateRoute path="profile" let:location>
+			<h3>Welcome {$user.username}</h3>
+			<button on:click={handleLogout}>Logout</button>
+		  </PrivateRoute>
+		</main>
+	  </Router>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/route/Login/Login.svelte
@@ -0,0 +1,38 @@
+<script>
+    import {
+        useNavigate,
+        useLocation
+    } from 'svelte-navigator';
+    import {
+        user
+    } from "$/stores";
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    let username;
+    let password;
+
+
+    function handleSubmit() {
+        $user = {
+            username,
+            password
+        };
+        const from = ($location.state && $location.state.from) || '/';
+        navigate(from, {
+            replace: true
+        });
+    }
+</script>
+
+<h3>
+    Login
+</h3>
+<form on:submit={handleSubmit}>
+    <input type="text" name="username" bind:value={username} />
+    <br />
+    <input type="password" name="password" bind:value={password} />
+    <br />
+    <button type="submit">Login</button>
+</form>
